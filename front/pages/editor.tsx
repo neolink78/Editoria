67603f1da55ef59d3ad5d4b80880222d00234242
T0MLE <lelaurain.tom@gmail.com>
2024-03-15 11:21:09 +0100

    Add new file tab and show/hide functionality for files in the project

--- front/pages/editor.tsx
+++ front/pages/editor.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Input } from "@chakra-ui/react";
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import Editor, { Monaco } from "@monaco-editor/react";
 import { useEffect, useState } from "react";
+import { BiChevronRight } from "react-icons/bi";
 
 type File = {
   name: string;
@@ -21,11 +22,16 @@ function CodeEditor() {
   const [output, setOutput] = useState<string>("");
   const [showInput, setShowInput] = useState<boolean>(false);
   const [newFileName, setNewFileName] = useState<string>("");
+  const [showTabs, setShowTabs] = useState({
+    files: false,
+    comments: false,
+    info: false,
+  });
 
   useEffect(() => {
     const file = project.find((file) => file.name === fileName);
     if (file) setSelectedFile(file);
-  }, [fileName, project])
+  }, [fileName, project]);
 
   useEffect(() => {
     console.log(project);
@@ -108,29 +114,54 @@ function CodeEditor() {
           direction={"column"}
           backgroundColor={"#212227"}
           color="white"
-          className="p-4"
         >
-          PROJECT
-          <Box>Info</Box>
-          <Box>Files</Box>
-          {project.map((file) => (
-            <p key={file.name} onClick={() => setFileName(file.name)}>
-              {file.name}
-            </p>
-          ))}
-          {showInput && (
-            <Input
-              placeholder="file name"
-              value={newFileName}
-              onChange={(e) => setNewFileName(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") addFile();
-              }}
+          <Text className="p-4">PROJECT</Text>
+          <Flex alignItems="center" bg="#2F3138" className="p-1 cursor-pointer">
+            <BiChevronRight />
+            Info
+          </Flex>
+          <Flex
+            alignItems="center"
+            bg="#2F3138"
+            className="p-1 cursor-pointer"
+            onClick={() => setShowTabs({ ...showTabs, files: !showTabs.files })}
+          >
+            <BiChevronRight
+              style={{ transform: showTabs.files ? "rotate(90deg)" : "" }}
             />
+            Files
+          </Flex>
+          {showTabs.files && (
+            <Flex direction={"column"} gap='2' className="px-8 py-2">
+              {project.map((file) => (
+                <p
+                  key={file.name}
+                  onClick={() => setFileName(file.name)}
+                  className="text-xs cursor-pointer"
+                >
+                  {file.name}
+                </p>
+              ))}
+              {showInput && (
+                <Input
+                  placeholder="file name"
+                  value={newFileName}
+                  onChange={(e) => setNewFileName(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") addFile();
+                  }}
+                  className="text-xs"
+                />
+              )}
+              {showTabs.files && (
+                <button className="m-auto" onClick={() => setShowInput(true)}>+</button>
+              )}
+            </Flex>
           )}
-          <button onClick={() => setShowInput(true)}>+</button>
-          <Box>Depedencies</Box>
-          <Box>Comments</Box>
+          <Flex alignItems="center" bg="#2F3138" className="p-1 cursor-pointer">
+            <BiChevronRight />
+            Comments
+          </Flex>
         </Flex>
         <Flex direction={"column"} w="100%">
           <Box backgroundColor={"#212227"} color="white">
@@ -140,7 +171,7 @@ function CodeEditor() {
                 disabled={fileName === file.name}
                 onClick={() => setFileName(file.name)}
                 className={
-                  "py-2 px-4 text-sm " +
+                  "py-2 px-4 text-sm cursor-pointer " +
                   (fileName === file.name ? "bg-[#14181F]" : "bg-[#25292F]") +
                   " hover:bg-[#14181F]"
                 }
