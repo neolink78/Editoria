cd51ae526fd7a3a207702b82b54c017dc80cfc0c
Omci <melloulchi.omar@gmail.com>
2024-04-07 16:14:28 +0200

    Add pagination to DashboardProject component

--- front/components/user/dashboardProjects.tsx
+++ front/components/user/dashboardProjects.tsx
@@ -1,5 +1,6 @@
 
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { SetStateAction, useState } from 'react';
 import ArrowLeftIcon from '../../icons/arrowLeftIcon';
 import Tile from '../../lib/tile';
 import { getLanguageIcon } from './dashboard';
@@ -19,7 +20,33 @@ interface DashboardProjectsProps {
 }
 
 const DashboardProjects = ({ projects, onDelete, setShowAllProjects }: DashboardProjectsProps) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const projectsPerPage = 8;
 
+    const indexOfLastProject = currentPage * projectsPerPage;
+    const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+    const currentProjects = projects.slice(indexOfFirstProject, indexOfLastProject);
+
+    const paginate = (pageNumber: SetStateAction<number>) => setCurrentPage(pageNumber);
+    const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+    const PaginationControls = () => (
+        <Flex mt="8" justifyContent="center" alignItems="center">
+            <Button onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))} disabled={currentPage === 1} mx="2">
+                Previous
+            </Button>
+            {Array.from({ length: totalPages }, (_, index) => (
+                <Button key={index} mx="1" onClick={() => paginate(index + 1)} variant={currentPage === index + 1 ? "solid" : "ghost"}>
+                    {index + 1}
+                </Button>
+            ))}
+
+            <Button onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages} mx="2">
+                Next
+            </Button>
+        </Flex>
+    );
+    
     return (
         <Box mb={10}>
             <Box
@@ -33,7 +60,7 @@ const DashboardProjects = ({ projects, onDelete, setShowAllProjects }: Dashboard
                 <Box display={"flex"} >
                     <ArrowLeftIcon onClick={() => setShowAllProjects(false)} /> Mes projets récents
                 </Box>
-                {projects.map((project, idx) => (
+                {currentProjects.map((project, idx) => (
                     <Tile
                         homePage={false}
                         projectId={project.id}
@@ -48,6 +75,7 @@ const DashboardProjects = ({ projects, onDelete, setShowAllProjects }: Dashboard
                     />
                 ))}
             </Box>
+            <PaginationControls />
         </Box>
     );
 };
