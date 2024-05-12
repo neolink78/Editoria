6bf6278b03484e81e516353327bed63382c541ce
Omci <melloulchi.omar@gmail.com>
2024-05-12 09:00:37 +0200

    feat: add description field to project entity
    Has been done to this PR already, needs to be merge first : https://github.com/neolink78/Editoria/pull/58

--- back/src/entities/project/project.ts
+++ back/src/entities/project/project.ts
@@ -1,5 +1,14 @@
 import { Field, ID, ObjectType } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import CodeSnippet from "../codeSnippet/codeSnippet";
 import User from "../user/user";
 import { CreateOrUpdateProjectArgs } from "./project.args";
@@ -8,40 +17,44 @@ type ProjectArgs = CreateOrUpdateProjectArgs & {
   owner: User;
 };
 
-
-//TODO: Add description to the project
-
 @Entity()
 @ObjectType()
- class Project extends BaseEntity {
+class Project extends BaseEntity {
   @PrimaryGeneratedColumn("uuid")
   @Field(() => ID)
-  id!: string
+  id!: string;
 
   @Column()
   @Field()
-  title!: string
+  title!: string;
+
+  @Column({ default: "" })
+  @Field()
+  description!: string;
 
   @Column({ default: true })
   @Field()
-  is_public!: boolean
+  is_public!: boolean;
 
   @CreateDateColumn()
   @Field()
   createdAt!: Date;
-  
+
   @CreateDateColumn()
   @Field()
   updatedAt!: Date;
 
-  @OneToMany(() => CodeSnippet, (codeSnippet) => codeSnippet.project, { eager: true, onDelete: "CASCADE" })
-  @Field(type => [CodeSnippet])
-  codeSnippetsOwned!: CodeSnippet[]
+  @OneToMany(() => CodeSnippet, (codeSnippet) => codeSnippet.project, {
+    eager: true,
+    onDelete: "CASCADE",
+  })
+  @Field((type) => [CodeSnippet])
+  codeSnippetsOwned!: CodeSnippet[];
 
   @ManyToOne(() => User, (user) => user.projectsOwned, { eager: true })
   @Field(() => User)
   owner!: User;
-  
+
   @ManyToMany(() => User, (collaborators) => collaborators.projects)
   collaborators!: User[];
 
@@ -50,6 +63,7 @@ type ProjectArgs = CreateOrUpdateProjectArgs & {
 
     if (project) {
       this.title = project.title;
+      this.description = project.description;
       this.is_public = project.is_public;
       this.owner = project.owner;
     }
@@ -57,7 +71,7 @@ type ProjectArgs = CreateOrUpdateProjectArgs & {
 
   static async createProject(project: ProjectArgs): Promise<Project> {
     const newProject = new Project(project);
-    
+
     return await Project.save(newProject);
   }
 
@@ -68,7 +82,7 @@ type ProjectArgs = CreateOrUpdateProjectArgs & {
   static async getProjectById(id: string): Promise<Project> {
     const project = await Project.findOne({ where: { id } });
     if (!project) {
-      throw new Error('Project not found');
+      throw new Error("Project not found");
     }
     return project;
   }
@@ -78,20 +92,24 @@ type ProjectArgs = CreateOrUpdateProjectArgs & {
     await Project.delete(id);
     return project;
   }
-  
-  static async updateProject(id: string, partialProject: CreateOrUpdateProjectArgs): Promise<Project> {
+
+  static async updateProject(
+    id: string,
+    partialProject: CreateOrUpdateProjectArgs
+  ): Promise<Project> {
     const project = await Project.getProjectById(id);
     Object.assign(project, partialProject, { updatedAt: new Date() });
-    
+
     if (partialProject.collaboratorIds) {
-      project.collaborators = await Promise.all(partialProject.collaboratorIds.map(User.getUserById));
+      project.collaborators = await Promise.all(
+        partialProject.collaboratorIds.map(User.getUserById)
+      );
     }
 
     await project.save();
-    project.reload()
+    project.reload();
     return project;
   }
+}
 
- }
-
- export default Project;
+export default Project;
