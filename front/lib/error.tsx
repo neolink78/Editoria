8d80855ce6588c666f468e24a9d104fc03de1f16
T0MLE <lelaurain.tom@gmail.com>
2024-05-15 15:41:05 +0200

    adjust margins and fontsize in  front/lib/error.tsx

--- front/lib/error.tsx
+++ front/lib/error.tsx
@@ -1,18 +1,21 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react";
 
 export const Error = () => {
-    return (
-        <Flex
-            direction="column"
-            align="center"
-            h="100vh"
-            textAlign="center"
-        >
-            <Box fontSize="1.4vw" mb="20px" mt="6rem">
-                Erreur !    </Box>
-            <Box fontSize="1.1vw" mb="40px" w="30vw">
-                Nous vous prions de nous excuser pour la gêne occasionnée. Veuillez réessayer plus tard.
-            </Box>
-        </Flex>
-    )
-}
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      h="100vh"
+      textAlign="center"
+      p={"40px"}
+    >
+      <Box fontSize={{ base: "32px", lg: "40px" }} mb="20px" mt="60px">
+        Une erreur est survenue.
+      </Box>
+      <Box fontSize={"16px"} w={{ base: "100%", lg: "30vw" }}>
+        Nous vous prions de nous excuser pour la gêne occasionnée. Veuillez
+        réessayer plus tard.
+      </Box>
+    </Flex>
+  );
+};
