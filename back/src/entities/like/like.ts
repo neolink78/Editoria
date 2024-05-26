f0a673d05a0d442ab2769a2d105d8f4c5b596968
Omci <melloulchi.omar@gmail.com>
2024-05-26 09:10:04 +0200

    fix: fix toggle like behavior
    - The toggle like functionality wasn't actually toggling and instead was constantly adding input in database.

--- back/src/entities/like/like.ts
+++ back/src/entities/like/like.ts
@@ -1,4 +1,10 @@
-import { BaseEntity, Entity, ManyToOne, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+import {
+  BaseEntity,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from "typeorm";
 import { Field, ID, ObjectType } from "type-graphql";
 import User from "../user/user";
 import Project from "../project/project";
@@ -10,17 +16,44 @@ class Like extends BaseEntity {
   @Field(() => ID)
   id!: string;
 
-  @ManyToOne(() => User, user => user.likedProjects)
+  @ManyToOne(() => User, (user) => user.likedProjects)
   @Field(() => User)
   user!: User;
 
-  @ManyToOne(() => Project, project => project.likedBy, { onDelete: "CASCADE" })
+  @ManyToOne(() => Project, (project) => project.likedBy, {
+    onDelete: "CASCADE",
+  })
   @Field(() => Project)
   project!: Project;
 
   @CreateDateColumn()
   @Field()
   createdAt!: Date;
+
+  static async toggleLike(userId: string, projectId: string): Promise<boolean> {
+    const userEntity = await User.findOne({ where: { id: userId } });
+    const projectEntity = await Project.findOne({ where: { id: projectId } });
+
+    if (!userEntity || !projectEntity) {
+      throw new Error("User or Project not found");
+    }
+
+    const like = await Like.findOne({
+      where: {
+        user: { id: userId },
+        project: { id: projectId },
+      },
+    });
+
+    if (like) {
+      await Like.remove(like);
+      return false;
+    } else {
+      const newLike = Like.create({ user: userEntity, project: projectEntity });
+      await newLike.save();
+      return true;
+    }
+  }
 }
 
 export default Like;
