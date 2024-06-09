e741e1b32321fb2bbb0e59fcb6fe2797fd027c9a
Omci <melloulchi.omar@gmail.com>
2024-06-09 19:08:49 +0200

    feat: add queries and mutations into separate files

---
+++ front/graphql/queries/likeQueries.ts
@@ -0,0 +1,18 @@
+import { gql } from "@apollo/client";
+
+export const GET_LIKED_PROJECTS = gql`
+query LikedProjects {
+  likedProjects {
+    id
+    title
+    description
+    owner {
+      id
+      username
+    }
+    likes {
+      id
+    }
+  }
+}
+`;
