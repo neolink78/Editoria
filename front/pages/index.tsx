95c8956c4ad52fbd38be53374d071543b4213c94
Omci <melloulchi.omar@gmail.com>
2024-06-14 10:14:56 +0200

    fix: fix tiles in homepage

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -4,19 +4,37 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import SubmitButton from "../lib/submitButton";
 import Tile from "../lib/tile";
-import indexMock from "../mocks/indexMock";
 import Layout from "../components/layout";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECTS } from "@/graphql/queries/projectQueries";
+import { useEffect } from "react";
+import { Error } from "@/lib/error";
 
 export default function HomePage() {
   const router = useRouter();
 
-  const { data, loading, error } = useQuery(GET_PROJECTS, {
+  const { data, loading, error, refetch } = useQuery(GET_PROJECTS, {
     variables: { limit: 5 },
   });
   const projects = data?.getProjects || [];
+  const sortedProjects = [...projects]
+    .sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    )
+    .slice(0, 3);
 
+  useEffect(() => {
+    refetch();
+  }, []);
+
+
+  const handleOpenProject = (projectId: string) => {
+    router.push(`/editor?project=${projectId}`);
+  };
+
+  if (loading) return <Layout>Loading...</Layout>;
+  if (error) return <Error></Error>
   return (
     <Layout>
       <Flex className="header_main_title">
@@ -52,15 +70,16 @@ export default function HomePage() {
         Most popular projects
       </Box>
       <Box ml="11.6vw">
-        {projects ? projects.slice(-5).map((e: any, idx: any) => (
+        {projects ? sortedProjects.slice(-5).map((e: any, idx: any) => (
           <Tile
             key={idx}
             icon={e.codeSnippetsOwned[0]?.language}
             title={e.title}
             description={e.description}
-            ownerId={e.owner.username}
+            owner={e.owner.username}
             createdAt={e.createdAt}
-            onOpenProject={() => router.push(`/projects/${e.projectId}`)}
+            onOpenProject={() => handleOpenProject(e.id)}
+            commentCount={e.comments.length}
             homePage
           />
         )) : null}
