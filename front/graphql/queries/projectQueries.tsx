4219fa5998044707675b12fd67b4a200ddc00c4a
Omci <melloulchi.omar@gmail.com>
2024-06-23 16:06:01 +0200

    feat: update GetProjects query to make offset and limit optional

--- front/graphql/queries/projectQueries.tsx
+++ front/graphql/queries/projectQueries.tsx
@@ -30,7 +30,7 @@ export const GET_USER_PROJECTS = gql`
 `;
 
 export const GET_PROJECTS = gql`
-  query GetProjects($offset: Int!, $limit: Int!) {
+  query GetProjects($offset: Int, $limit: Int) {
     getProjects(offset: $offset, limit: $limit) {
       projects {
         id
