029061d2bb7fc3f524cdebb2ddd270fada316ec3
Omci <melloulchi.omar@gmail.com>
2024-07-19 20:32:02 +0200

    feat: add search to gql query

--- front/graphql/queries/projectQueries.tsx
+++ front/graphql/queries/projectQueries.tsx
@@ -30,8 +30,8 @@ export const GET_USER_PROJECTS = gql`
 `;
 
 export const GET_PROJECTS = gql`
-  query GetProjects($limit: Int!, $offset: Int!, $sortBy: String!) {
-    getProjects(offset: $offset, limit: $limit, sortBy: $sortBy) {
+  query GetProjects($limit: Int!, $offset: Int!, $sortBy: String!, $search: String) {
+    getProjects(offset: $offset, limit: $limit, sortBy: $sortBy, search: $search) {
       projects {
         id
         title
