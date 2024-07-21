0f563c5fe257cb5cc09a4f6197a60ef3857c08ba
Omci <melloulchi.omar@gmail.com>
2024-07-21 11:55:30 +0200

    style: fix style for comment card

--- front/lib/commentCard.tsx
+++ front/lib/commentCard.tsx
@@ -10,6 +10,7 @@ interface CommentCardProps {
 const CommentCard = ({ title, date, content, onOpenProject }: CommentCardProps) => {
   return (
     <Box
+      m="4"
       p="4"
       mb="4"
       borderWidth="1px"
@@ -19,13 +20,13 @@ const CommentCard = ({ title, date, content, onOpenProject }: CommentCardProps) 
       _hover={{ boxShadow: "lg", cursor: "pointer" }}
       onClick={onOpenProject}
     >
-      <Text fontWeight="bold" fontSize="1.2rem" mb="2">
+      <Text fontWeight="bold" fontSize="1.2rem" mb="2" >
         {title}
       </Text>
       <Text fontSize="1rem">
         {content}
       </Text>
-      <Text fontSize="0.9rem" color="gray.500" mb="4">
+      <Text fontSize="0.8rem" color="gray.500" mb="4">
         {date}
       </Text>
     </Box>
