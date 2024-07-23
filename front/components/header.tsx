add501b64019d81a61b5fd587d4070ec287329c1
T0MLE <lelaurain.tom@gmail.com>
2024-07-23 16:36:00 +0200

    [refacto] - add user popin nav to dashboard or sign out

--- front/components/header.tsx
+++ front/components/header.tsx
@@ -1,14 +1,20 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import UserIcon from "../icons/userIcon";
 import LogOutIcon from "../icons/logOutIcon";
 import SubmitButton from "../lib/submitButton";
 import { useAuth } from "../context/UserContext";
+import { useEffect, useRef, useState } from "react";
+import { isClickOutside } from "../utils/event";
 
 const Header = () => {
   const router = useRouter();
   const { user, signOut } = useAuth();
+  const [openModal, setOpenModal] = useState(false);
+  const modalRef = useRef<HTMLInputElement | null>(null);
+  const userIconRef = useRef<HTMLInputElement | null>(null);
+
 
   const handleSignOut = async () => {
     try {
@@ -20,6 +26,24 @@ const Header = () => {
   };
 
   const projectsPage = router.pathname === "/projects";
+
+    /**
+   * Close modal when clicking outside
+   * @param event
+   */
+    const clickOutsideHandler = (event: MouseEvent) => {
+      if (openModal && modalRef && isClickOutside(event, modalRef.current) && isClickOutside(event, userIconRef.current)) {
+        setOpenModal(false);
+      }
+    };
+
+    useEffect(() => {
+      document.addEventListener("mousedown", clickOutsideHandler);
+  
+      return () => {
+        document.removeEventListener("mousedown", clickOutsideHandler);
+      };
+    });
 
   return (
     <Flex className="header_home_page">
@@ -41,16 +65,60 @@ const Header = () => {
         </SubmitButton>
         {user ? (
           <>
-            <Box onClick={() => router.push("/user/account")} cursor="pointer">
+            <Box
+              ref={userIconRef}
+              onClick={() => setOpenModal((modal) => !modal)}
+              cursor="pointer"
+            >
               <UserIcon />
-            </Box>
-            <Box onClick={handleSignOut} cursor="pointer">
-              <LogOutIcon />
             </Box>
           </>
         ) : (
           ""
         )}
+      </Flex>
+      <Flex
+        ref={modalRef}
+        direction="column"
+        className={openModal ? "user_modal -is-visible" : "user_modal"}
+      >
+        <Text
+          fontSize="md"
+          onClick={() => router.push("/user/account?tab=dashboard")}
+          cursor={"pointer"}
+          className="mb-1 p-1 font-semibold hover:bg-[#575d64] rounded transition-all"
+        >
+          Dashboard
+        </Text>
+        <Text
+          fontSize="md"
+          onClick={() => router.push("/user/account?tab=settings")}
+          cursor={"pointer"}
+          className="mb-1 p-1 font-semibold hover:bg-[#575d64] rounded transition-all"
+        >
+          Settings
+        </Text>
+        <Text
+          fontSize="md"
+          onClick={() => router.push("/user/account?tab=yourfavcoders")}
+          cursor={"pointer"}
+          className="mb-2 p-1 font-semibold hover:bg-[#575d64] rounded transition-all"
+        >
+          Your favorite coders
+        </Text>
+        <Box className="border-b border-[#575d64] w-full" />
+        <Flex
+          onClick={handleSignOut}
+          cursor="pointer"
+          gap={3}
+          className="mt-2 p-1 hover:bg-[#575d64] rounded transition-all"
+          alignItems="center"
+        >
+          <Text fontSize="md" className="font-semibold">
+            Sign out
+          </Text>
+          <LogOutIcon />
+        </Flex>
       </Flex>
     </Flex>
   );
