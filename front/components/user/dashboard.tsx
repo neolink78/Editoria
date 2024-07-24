a0e3e674669388fc8347bc3b2dda991595c44cb3
Omci <melloulchi.omar@gmail.com>
2024-07-24 10:40:22 +0200

    fix: fix style and typo in dashboard and settings

--- front/components/user/dashboard.tsx
+++ front/components/user/dashboard.tsx
@@ -300,7 +300,7 @@ const Dashboard = () => {
               )}
             </Box>
             <Box
-              fontSize="1rem"
+              fontSize={{ base: "1rem", sm: "0.8rem", md: "1.2rem", lg: "1.4rem" }}
               m={"2vw 0 1rem 10vw"}
               display="flex"
               alignItems="baseline"
--- front/components/user/settings.tsx
+++ front/components/user/settings.tsx
@@ -125,7 +125,7 @@ const Settings = (user: any) => {
         />
         Your informations
         <SubmitButton onClick={() => editSettings()}>
-          {isDisabled ? "Edit" : "save"}
+          {isDisabled ? "Edit" : "Save"}
         </SubmitButton>
       </Flex>
       <SettingsInput
